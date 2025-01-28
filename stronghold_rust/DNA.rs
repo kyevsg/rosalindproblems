161a56ca7c7e6185bc4a4838b0c333d86ad91a00
Kye Stapleton-Gray <88254707+kyevsg@users.noreply.github.com>
2025-01-28 17:19:14 -0500

    Update DNA.rs

--- stronghold_rust/DNA.rs
+++ stronghold_rust/DNA.rs
@@ -9,8 +9,8 @@ fn dna_count(dna_string: &str) {
     Input: a DNA string
     Returns: the counts of A, C, G, and T within the DNA string as four integers separated by spaces
     */
-    let mut nuc_map = HashMap::new();
-    for nuc in dna_string.chars() {
+    let mut nuc_map = HashMap::new(); // creating hash map to store nucleotide counts
+    for nuc in dna_string.chars() { // iterating over DNA string 
         let counter = nuc_map.entry(nuc).or_insert(0);
         *counter += 1
     }
