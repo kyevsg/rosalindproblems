c46238d66979a2253760970b71032c7d4d1af9ee
Kye Stapleton-Gray <88254707+kyevsg@users.noreply.github.com>
2025-01-28 17:29:24 -0500

    Create RNA.rs

---
+++ stronghold_rust/RNA.rs
@@ -0,0 +1,16 @@
+// Transcribing DNA into RNA
+
+fn transcription(dna_string: &str) {
+    /*
+    This function transcribes a DNA sequence to an RNA one.
+
+    Input: a DNA string
+    Prints: the corresponding RNA string
+    */
+    let rna_string = dna_string.replace("T", "U"); // replace T nucleotides with U
+    print!("{}", rna_string);
+}
+
+fn main() {
+    transcription(); // enter DNA string here
+}
