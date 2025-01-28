cdabc2c863c40dc71c3f14ee1cdcb624a3bbfa23
Kye Stapleton-Gray <88254707+kyevsg@users.noreply.github.com>
2025-01-28 14:23:59 -0500

    Working on DNA challenge in rust

--- stronghold_rust/DNA.rs
+++ stronghold_rust/DNA.rs
@@ -1,10 +1,22 @@
-//
+// Counting DNA Nucleotides
 
+use std::collections::HashMap;
 
+fn dna_count(dna_string: &str) {
+    /*
+    This function counts the number of each type of nucleotide within a DNA sequence.
 
+    Input: a DNA string
+    Returns: the counts of A, C, G, and T within the DNA string as four integers separated by spaces
+    */
+    let mut nuc_map = HashMap::new();
+    for nuc in dna_string.chars() {
+        let counter = nuc_map.entry(nuc).or_insert(0);
+        *counter += 1
+    }
+    nuc_map
+}
 
-
-fn dna_count() {
-    let input = ;
-    let output = count(input);
+fn main() {
+    dna_count(); // enter DNA string here
 }
