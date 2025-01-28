debf52cb022013d1b6e1f4626687868408cc9bf6
Kye Stapleton-Gray <88254707+kyevsg@users.noreply.github.com>
2025-01-28 17:48:16 -0500

    Working on reverse complement function

---
+++ stronghold_rust/REVC.rs
@@ -0,0 +1,17 @@
+// Complementing a Strand of DNA
+
+fn transcription(dna_string: &str) {
+    /*
+    This function returns the reverse complement of a DNA sequence.
+
+    Input: a DNA string
+    Prints: the reverse complement string
+    */
+    let rev_string = reverse_string(dna_string); // reverses the dna string
+    let comp_string = ; // replace bases with their complements
+    print!("{}", comp_string);
+}
+
+fn main() {
+    transcription(); // enter DNA string here
+}
